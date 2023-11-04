3886d0627907b3b8e683a7ec6288ed2bf65a082e
Carlos Vargas <desarrolloarve@gmail.com>
2023-11-04 11:08:34 -0400

    validando el error de descontar creditos

---
+++ database/migrations/2023_11_03_155852_add_ip_field_in_demos_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddIpFieldInDemosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('demos', function (Blueprint $table) {
+            $table->string("ip_address",100)->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('demos', function (Blueprint $table) {
+            //
+        });
+    }
+}
